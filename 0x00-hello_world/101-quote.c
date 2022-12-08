85f60969e432eecb7d0d9cd99f44c6a9ed3beebc
xhepan <labs6068@gmail.com>
2022-12-08 13:25:20 -0800

    error msg with stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints error msg
+ *
+ * Return: 1 since code unsuccessful
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
