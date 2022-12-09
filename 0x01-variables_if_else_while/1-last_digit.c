dcc2a38465105fa31c76ab45fac0053a650f4421
xhepan <labs6068@gmail.com>
2022-12-09 15:08:31 -0800

    check if num is >5, <6 or zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,11 @@
  */
 int main(void)
 {
-	int n;
+	int n, ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	int ld; 
+	/* your code goes there */ 
 	ld = n % 10;
 	if (ld > 5)
 	{
