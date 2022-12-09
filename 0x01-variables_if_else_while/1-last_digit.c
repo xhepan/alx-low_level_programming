91980eb7466cb212891ef1dbe49cae4fef03977a
xhepan <labs6068@gmail.com>
2022-12-09 15:06:57 -0800

    check if num is >5, <6 or zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int ld = n % 10;
+	int ld; 
+	ld = n % 10;
 	if (ld > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
