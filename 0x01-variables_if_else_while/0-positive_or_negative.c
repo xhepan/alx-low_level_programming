1d87c7d09ea72f48bd915b44dc8a57a27875241b
xhepan <labs6068@gmail.com>
2022-12-09 14:46:51 -0800

    check if num is neg, pos or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Checks if number is positive, negative or zero
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
 	int n;
@@ -10,5 +15,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
