b342b857e78d6da20890ccf365a9138c5c6d2473
xhepan <102022513+xhepan@users.noreply.github.com>
2022-12-10 01:53:14 +0200

    w/o q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main -Print alphabet in lowercase
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char alph[50] = {"abcdfghijklmnoprstuvwxyz"};
+	int i, len;
+
+	i = 0;
+	len = strlen(alph);
+
+	while (i < len)
+	{
+		putchar(alph[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
