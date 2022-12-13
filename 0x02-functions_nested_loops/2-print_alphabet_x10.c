836eed7083e15abed3eec2d5e4c0eb29ffd3fa48
xhepan <102022513+xhepan@users.noreply.github.com>
2022-12-14 00:11:45 +0200

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_alphabet - uses _putchar to print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char c;
+  int i;
+
+  for (i = 0; i < 10; i++)
+  {
+    for (c = 'a'; c <= 'z'; c++)
+    {
+      _putchar(c);
+    }
+	  _putchar('\n');
+  }
+  _putchar('\n');
+}
