73a0fc6e3134a6d65f8927abef0ffbd696f563df
xhepan <102022513+xhepan@users.noreply.github.com>
2022-12-14 00:15:30 +0200

    print alphabet 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,13 @@
 void print_alphabet(void);
 
 /**
+ * print_alphabet_x10 - uses _putchar() to print alphabet 10 times
+ *
+ * No return ~ void
+ */
+void print_alphabet_x10(void);
+
+/**
  * _putchar - prints
  *
  * No return
