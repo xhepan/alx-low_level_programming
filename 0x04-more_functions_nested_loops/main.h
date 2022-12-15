316c8f85d29a4efae4c76413e8ff316af61bb24b
xhepan <labs6068@gmail.com>
2022-12-15 13:32:55 -0800

    print digits using putchar except 2,4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -22,3 +22,8 @@ void print_numbers(void);
  * _putchar - uses putchar to print a character
  */
 void _putchar(int c);
+
+/**
+ * print_most_numbers - print digits 0-9 except 2,4
+ */
+void print_most_numbers(void);
