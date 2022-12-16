9ec7da971b8997a324dfdeac99695e5ff1775a44
xhepan <labs6068@gmail.com>
2022-12-15 23:25:11 -0800

    remaining days of the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,8 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0 && year % 10 == 0))
+	else if ((year % 4 == 0 || year % 400 == 0)
+	&& (year % 100 == 0 && year % 10 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
