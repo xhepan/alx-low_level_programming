48e05a583fa7442636d0f93f4ca5b94dc5cfa3c1
xhepan <labs6068@gmail.com>
2022-12-20 01:48:51 -0800

    print strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints string
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,3 +17,8 @@ void swap_int(int *a, int *b);
  * _strlen - returns length of string
  */
 int _strlen(char *s);
+
+/**
+ * _puts - uses puts to print string
+ */
+void _puts(char *str);
