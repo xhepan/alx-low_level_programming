8a8afb08e096bcfea612ece9a79a98e9cd3096b3
xhepan <labs6068@gmail.com>
2022-12-20 01:39:45 -0800

    get string length using pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - checks length of string
+ * @s: string to be checked
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,3 +12,8 @@ void reset_to_98(int *n);
  * swap_int - swaps ints
  */
 void swap_int(int *a, int *b);
+
+/**
+ * _strlen - returns length of string
+ */
+int _strlen(char *s);
