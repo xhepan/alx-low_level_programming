dd6730f72c3aeb40ff1196d4876d4b4811920a95
xhepan <labs6068@gmail.com>
2022-12-27 16:09:33 -0800

    print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * times_table - print times table 0-9
@@ -13,7 +14,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			p = i * j;
-			_putchar(p % 10 + '0');
+			printf("%d", p);
 			if (j != 9)
 			{
 				_putchar(',');
