0afdfceda75e379179dcc34286e95c5d16255015
xhepan <labs6068@gmail.com>
2023-01-04 06:05:29 -0800

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.c>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: value of comparison :)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,8 @@ char *_strncat(char *dest, char *src, int n);
  * _strncpy - copies string
  */
 char *_strncpy(char *dest, char *src, int n);
+
+/**
+ * _strcmp - compares two strings
+ */
+int _strcmp(char *s1, char *s2);
